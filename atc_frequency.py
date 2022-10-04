1dd44b8614fc21408db5f630c93ea6946abb2d3b
Nash115 <72687027+Nash115@users.noreply.github.com>
2022-10-04 08:23:30 +0200

    Version V0.6.0-2022-10-04

---
+++ atc_frequency.py
@@ -0,0 +1,16 @@
+# Version V0.6.0-2022-10-04
+
+from SimConnect import *
+
+sm = SimConnect()
+aq = AircraftRequests(sm, _time=2000)
+
+def getFrequency(frec):
+    if aq.get("COM_ACTIVE_FREQUENCY:1") != None:
+        frequency = str(round(aq.get("COM_ACTIVE_FREQUENCY:1"), 4))
+        while len(frequency)<6:
+            frequency += "0"
+        return frequency
+    else:
+        print("Frquence introuvable, paramétrage de la dernière fréquence")
+        return frec

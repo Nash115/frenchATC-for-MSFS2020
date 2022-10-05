8cb1faeb2221a3bdc946502806dfa06beb596553
Nash115 <72687027+Nash115@users.noreply.github.com>
2022-10-05 20:43:25 +0200

    libraries_installer.bat ajouté

---
+++ libraries_installer.bat
@@ -0,0 +1,25 @@
+echo off
+
+cls
+
+pip install argparse
+echo ####################
+pip install sounddevice
+echo ####################
+pip install vosk
+echo ####################
+pip install colorama
+echo ####################
+pip install gtts
+echo ####################
+pip install requests
+echo ####################
+pip install bs4
+echo ####################
+pip install SimConnect
+
+cls
+
+echo Programme termine, les librairies ont du s'installer avec succes
+
+pause

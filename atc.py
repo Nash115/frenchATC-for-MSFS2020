00ede8860ab858cb5e876acc17daa3a9f78b36e3
Nash115 <hugomr83500@gmail.com>
2022-12-10 01:04:54 +0100

    Crash correction

--- atc.py
+++ atc.py
@@ -72,7 +72,7 @@ aff.display(Fore.GREEN +' Démarrage des services en cours... Veuillez patienter
 
 def callback(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
-    if status:
+    #if status:
         #print(status, file=sys.stderr)
     q.put(bytes(indata))
 
